e09056cc56c8764ef7f3f46fe851889cb406e14f
Larissa <larissasousa.wrks@gmail.com>
2024-07-15 10:01:32 -0400

    init springb config

---
+++ src/test/java/com/challenge/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
